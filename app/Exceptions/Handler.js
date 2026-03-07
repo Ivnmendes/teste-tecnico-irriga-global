096f67312536d85552f8ebb6c62056b55831ab24
Ivan <ivanmendes2005@gmail.com>
2026-03-07 09:59:45 -0300

    feat: improve global exception handler

--- app/Exceptions/Handler.js
+++ app/Exceptions/Handler.js
@@ -23,11 +23,30 @@ class ExceptionHandler extends BaseExceptionHandler {
   async handle (error, { request, response }) {
 
     if (error.name === 'ValidationException') {
-      const formattedErrors = error.messages.map(err => ({
-        message: err.message
-      }))
-      
-      return response.status(error.status).json(formattedErrors)
+      const formattedErrors = error.messages.reduce((acc, curr) => {
+        acc[curr.field] = curr.message
+        return acc
+      }, {})
+
+      return response.status(error.status).json({
+        errors: formattedErrors
+      })
+    }
+
+    if (error.name === 'InvalidJwtToken' || error.code === 'E_INVALID_JWT_TOKEN') {
+      return response.status(401).json({
+        message: 'Erro: Token JWT inválido ou ausente.'
+      })
+    }
+
+    if (
+      error.code === 'E_ROUTE_NOT_FOUND' || 
+      error.code === 'E_ROW_NOT_FOUND' || 
+      error.name === 'ModelNotFoundException'
+    ) {
+      return response.status(404).json({
+        message: 'O recurso solicitado não existe ou você não tem permissão para acessá-lo.'
+      })
     }
 
     return response.status(500).json({
